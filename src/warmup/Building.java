228a57ca412a4bd78b4adbad1b002f90a1a87885
alamaabreh <alamaabreh@10.0.0.193>
2018-08-12 15:05:30 -0400

    new class

---
+++ src/warmup/Building.java
@@ -0,0 +1,60 @@
+package warmup;
+
+public class Building {
+ 
+private String name;
+private String type;
+private int floorCount;
+
+public Building(){
+	System.out.println("no args");
+}
+
+
+
+public Building(String name, String type, int floorCount) {
+	super();
+	this.name = name;
+	this.type = type;
+	this.floorCount = floorCount;
+}
+ @Override
+	public String toString() {
+		return"building info: " + name+" " + type+ " "+floorCount;
+		
+	} 
+
+public void addFloor(){
+	floorCount++;
+	System.out.println("floor count increased");
+}
+public void addDoor(){
+	System.out.println("floor count increased");
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getType() {
+	return type;
+}
+public void setType(String type) {
+	this.type = type;
+}
+public int getFloorCount() {
+	return floorCount;
+}
+public void setFloorCount(int floorCount) {
+	this.floorCount = floorCount;
+}
+
+
+}
+
+	
+	
+	
+	
+	
